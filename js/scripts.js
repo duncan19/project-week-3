c0d277c64ede227d80884cd322601837e0932e9b
Duncan Robbins <duncanrobbins19@gmail.com>
2020-01-24 13:09:17 -0800

    allow the program to see if the user's entry includes a three

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-
 var arrayOfUserNUm=[];
+
 
 $(document).ready(function(){
   $("#userNumForm").submit(function(event){
     event.preventDefault();
     $("#userNumForm").hide();
     var usersNumber = parseInt($("input#userinputtedNumber").val());
-})
+
+      var moveToArrayFunc  = function(){
+
+      var usersnumchange = usersNumber
+      for(i=1; i <= usersNumber; i++){
+        arrayOfUserNUm.push(usersnumchange)
+        usersnumchange -= 1;
+        }
+        for (i = 0; i < arrayOfUserNUm.length; i ++){
+         if(arrayOfUserNUm[i].toString().includes(3))
+         {
+                arrayOfUserNUm.push('Im sorry I can not do that')
+         }
+
+};
+
+});
 });
