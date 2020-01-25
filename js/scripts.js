30e5826c01aad49031d5b31741d90e89b3185eb4
Duncan Robbins <duncanrobbins19@gmail.com>
2020-01-24 16:40:52 -0800

    start bug fixing for why result wont display

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 var arrayOfUserNUm=[];
+
+
 $(document).ready(function(){
   $("#userNumForm").submit(function(event){
     event.preventDefault();
     $("#userNumForm").hide();
     var usersNumber = parseInt($("input#userinputtedNumber").val());
+
       var moveToArrayFunc  = function(){
+
       var usersnumchange = usersNumber
       for(i=1; i <= usersNumber; i++){
         arrayOfUserNUm.push(usersnumchange)
@@ -26,6 +30,11 @@ $(document).ready(function(){
          else{
            arrayOfUserNUm.push(arrayOfUserNUm[i])
          }
+       }
+       var arrayforresult = arrayOfUserNUm.toString();
+
+       $(".result-area").text(arrayforresult)
+       $(".result").show();
 };
 
 });
